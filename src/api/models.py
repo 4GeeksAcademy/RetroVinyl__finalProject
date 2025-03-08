1f970b2d5372c8d525d18a0c618d5088f2c234fb
Dmm91 <moya91.dmm@gmail.com>
2025-03-08 00:04:06 +0000

    Solucionado agregar y borrar favs

--- src/api/models.py
+++ src/api/models.py
@@ -39,5 +39,29 @@ class Album(db.Model):
             "have" : self.have,   
         }
     
+class Favoritos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_album =db.Column(db.Integer, db.ForeignKey('album.id'))
+    title = db.Column(db.String(250), unique=False, nullable=False)
+    country = db.Column(db.String(80), unique=False, nullable=False)
+    year = db.Column(db.String(80), unique=False, nullable=False)
+    cover_image = db.Column(db.String(250), unique=False, nullable=False)
+    genre = db.Column(db.String(250), unique=False, nullable=False)
+    have = db.Column(db.String(80), unique=False, nullable=False)    
+    
+    def __repr__(self):
+        return f'<Favoritos {self.title}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_album" :self.id_album,
+            "title": self.title,
+            "country" : self.country,
+            "year" : self.year,
+            "cover_image" : self.cover_image,
+            "genre" : self.genre,
+            "have" : self.have,   
+        }
     
     
--- src/front/js/pages/despliegue_albums.js
+++ src/front/js/pages/despliegue_albums.js
@@ -1,66 +1,107 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import "../../styles/despliegue_albums.css";
 import { useParams } from "react-router-dom";
+import "../../styles/despliegue_albums.css";
 
 export const DespliegueAlbums = () => {
-    const {decada, genero} = useParams()
+    const { decada, genero } = useParams();
+    const [albums, setAlbums] = useState([]);
+    const [favoritos, setFavoritos] = useState([]);
 
-    const [albums, setAlbums] = useState([])
+    const getFavoritos = async () => {
+        const response = await fetch(`${process.env.BACKEND_URL}/api/favoritos`);
+        const data = await response.json();
+        setFavoritos(data);
+    };
+    useEffect(() => {
+        const getAlbums = async () => {
+            const response = await fetch(`${process.env.BACKEND_URL}/api/albums/${decada}/${genero}`);
+            const data = await response.json();
+            setAlbums(data);
+        };
 
-    useEffect(()=>{ // DONDE LLAMO A LA RUTA DEL BACK QUE TRAE LA INFORMACION??????
-        console.log("estoy cargando los albumes");
-        
-        const getAlbums = async () =>{
-            const response = await fetch(`${process.env.BACKEND_URL}/api/albums/${decada}/${genero}`)
-            const data = await response.json()
-            setAlbums(data)
+        getAlbums();
+        getFavoritos();
+    }, []);
 
+    // Añadir un favorito
+    const postFavorito = async (album) => {
+        const response = await fetch(`${process.env.BACKEND_URL}/api/favoritos`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(album),
+        });
+        if (response.ok) {
+            console.log("Álbum añadido");
+            getFavoritos()
         }
-        getAlbums()
-    },[decada, genero])
+    };
+    // Eliminar un favorito
+    const deleteFavorito = async (album) => {
+        const response = await fetch(`${process.env.BACKEND_URL}/api/favoritos/${album}`, {
+            method: 'DELETE'
+        });
+        if (response.ok) {
+            console.log("Álbum borrado");
+            getFavoritos()
+        }
+    };
+    //Comprueba en el useState si el album existe en favoritos
+    const isFavorito = (id) => {
+         console.log("IDs en favoritos:", favoritos.map(f => f.id));
+        return favoritos.some((fav) => fav.id === id);
+    };
+
+    // Maneja el clic en el botón favorito (Si el album no existe, lo añade. Si existe, lo borra)
+    const handleFavoritoClick = (album) => {
+        if (isFavorito(album.id)) {
+            deleteFavorito(album.id);
+        } else {
+            postFavorito(album);
+        }
+    };
     return (
         <div className="cont-gen container">
             <div className="titulo-gen">
-                <h1 className="nombre-genero text-center my-2"><span>{decada}</span>'S <span className="text-light">{genero.toUpperCase()}</span></h1>
+                <h1 className="nombre-genero text-center my-2">
+                    <span>{decada}</span>'S <span className="text-light">{genero.toUpperCase()}</span>
+                </h1>
             </div>
             <div className="cont-gen-card row">
-                {
-                    albums.map((album)=>{
-                        return (
-                            <div className="carta-gen">
-                            <div className="img-gen-cont">
-                                <img className="img-gen" src={album.cover_image} />
-        
-                            </div>
-                            <div className="cuerpo-gen">
-                                <div className="detalles-gen">
-                                    <p>Título: {album.title}</p>
-                                    <p>País: {album.country} </p>
-                                    <p>Año: {album.year} </p>
-                                </div>
-                            </div>
-                            <div className="cont-gen-btn">
-                                <a className="youtube-gen d-flex justify-content-center align-items-center text-decoration-none"
-                                    href="https://www.youtube.com/watch?v=pXu6JC6-d_o&list=PL7tBPYQzCjeLGK4hm4eZnf1gQRPQZYg8Y"
-                                    target="_blank"
-                                    rel="noopener noreferrer">
-                                    <i class="fa-brands fa-youtube" style={{ color: '#f20d0d' }}></i>
-                                </a>
-                                <button className="fav-gen">
-                                    <i class="fa-solid fa-star" style={{ color: '#daab03' }}></i>
-                                </button>
-                                <button className="compra-gen">
-                                    <i class="fa-solid fa-cart-shopping" style={{ color: '#248cdb' }}></i>
-                                </button>
+                {albums.map((album) => (
+                    <div className="carta-gen" key={album.id}>
+                        <div className="img-gen-cont">
+                            <img className="img-gen" src={album.cover_image} alt={album.title} />
+                        </div>
+                        <div className="cuerpo-gen">
+                            <div className="detalles-gen">
+                                <p>Título: {album.title}</p>
+                                <p>País: {album.country}</p>
+                                <p>Año: {album.year}</p>
                             </div>
                         </div>
-                        )
-                    })
-                }
+                        <div className="cont-gen-btn">
+                            <a
+                                className="youtube-gen d-flex justify-content-center align-items-center text-decoration-none"
+                                href="https://www.youtube.com/watch?v=pXu6JC6-d_o&list=PL7tBPYQzCjeLGK4hm4eZnf1gQRPQZYg8Y"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                <i className="fa-brands fa-youtube" style={{ color: '#f20d0d' }}></i>
+                            </a>
+                            <button className="fav-gen" onClick={() => handleFavoritoClick(album)}>{
+                                isFavorito(album.id)
+                                    ?
+                                    <i class="fa-solid fa-star" style={{ color: '#daab03' }}></i>
+                                    :
+                                    <i class="fa-regular fa-star" style={{ color: '#daab03' }}></i>
+                            }
+                            </button>
+                            <button className="compra-gen">
+                                <i className="fa-solid fa-plus" style={{ color: '#248cdb' }}></i>
+                            </button>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
-    )
-}
-
-
+    );
+};
