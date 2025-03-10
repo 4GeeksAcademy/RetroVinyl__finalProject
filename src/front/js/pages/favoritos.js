73b70b50b053679992f758d3a53940e5e7d05c75
Dmm91 <moya91.dmm@gmail.com>
2025-03-10 18:44:28 +0000

    Trayendo info a pestaña favoritos

--- src/front/js/pages/favoritos.js
+++ src/front/js/pages/favoritos.js
@@ -1,110 +1,51 @@
-import React,{useContext, useEffect} from "react";
-import { Context } from "../store/appContext";
+import React, { useState, useEffect } from "react";
 import "../../styles/favoritos.css";
+
 export const Favoritos = () => {
-    
-    
+
+    const [favoritos, setFavoritos] = useState([]);
+    console.log(favoritos);
+    const getFavoritos = async () => {
+        const response = await fetch(`${process.env.BACKEND_URL}/api/favoritos`);
+        const data = await response.json();
+        setFavoritos(data);
+        console.log(data);
+
+    };
+    useEffect(() => {
+        getFavoritos()
+    }, []);
+
     return (
-        <div className="cont-favs container">
-            <div className="titulo-favs">
-                <h1 className="">Mis Favoritos</h1>
-            </div>
-            <div className="card-cont-fav">
-                <div className="carta-favs">
-                    <div className="img-favs-cont">
-                        <img className="img-favs" src="https://m.media-amazon.com/images/I/913fRR9Dk5L._UF894,1000_QL80_.jpg" />
-                    </div>
-                    <div className="cuerpo-favs">
-                        <h4>Título: Origins</h4>
-                        <p>Artista: Imagine Dragons </p>
-                        <p>Año: 2018 </p>
-                        <button type="button" className="genero-favs btn btn-danger">Pop-Rock</button>
-                    </div>
-                    <div className="botones-cont1">
-                        <div className="botones-cont2">
-                            <button className="fav-favs">
-                                <i class="fa-solid fa-star" style={{ color: '#daab03' }}></i>
-                            </button>
+        <div className="card-cont-fav">
+            {favoritos.map((item) => {
+                const album = item.album; // Extraer el álbum del objeto
+                return (
+                    <div className="carta-favs" key={album.id}>
+                        <div className="img-favs-cont">
+                            <img className="img-favs" src={album.cover_image} alt={album.title} />
                         </div>
-                        <div className="botones-cont3">
-                            <button className="compra-favs">
-                                <i class="fa-solid fa-cart-shopping" style={{ color: '#248cdb' }}></i>
-                            </button>
+                        <div className="cuerpo-favs">
+                            <p>Título: {album.title}</p>
+                            <p>Artista: {album.have}</p>
+                            <p>Año: {album.year}</p>
+                            <button type="button" className="genero-favs btn btn-danger">{album.genre}</button>
                         </div>
-
-                    </div>
-                </div>
-                <div className="carta-favs">
-                    <div className="img-favs-cont">
-                        <img className="img-favs" src="https://m.media-amazon.com/images/I/913fRR9Dk5L._UF894,1000_QL80_.jpg" />
-                    </div>
-                    <div className="cuerpo-favs">
-                        <h4>Título: Origins</h4>
-                        <p>Artista: Imagine Dragons </p>
-                        <p>Año: 2018 </p>
-                        <button type="button" className="genero-favs btn btn-danger">Pop-Rock</button>
-                    </div>
-                    <div className="botones-cont1">
-                        <div className="botones-cont2">
-                            <button className="fav-favs">
-                                <i class="fa-solid fa-star" style={{ color: '#daab03' }}></i>
-                            </button>
-                        </div>
-                        <div className="botones-cont3">
-                            <button className="compra-favs">
-                                <i class="fa-solid fa-cart-shopping" style={{ color: '#248cdb' }}></i>
-                            </button>
-                        </div>
-
-                    </div>
-                </div>
-                <div className="carta-favs">
-                    <div className="img-favs-cont">
-                        <img className="img-favs" src="https://m.media-amazon.com/images/I/913fRR9Dk5L._UF894,1000_QL80_.jpg" />
-                    </div>
-                    <div className="cuerpo-favs">
-                        <h4>Título: Origins</h4>
-                        <p>Artista: Imagine Dragons </p>
-                        <p>Año: 2018 </p>
-                        <button type="button" className="genero-favs btn btn-danger">Pop-Rock</button>
-                    </div>
-                    <div className="botones-cont1">
-                        <div className="botones-cont2">
-                            <button className="fav-favs">
-                                <i class="fa-solid fa-star" style={{ color: '#daab03' }}></i>
-                            </button>
-                        </div>
-                        <div className="botones-cont3">
-                            <button className="compra-favs">
-                                <i class="fa-solid fa-cart-shopping" style={{ color: '#248cdb' }}></i>
-                            </button>
+                        <div className="botones-cont1">
+                            <div className="botones-cont2">
+                                <button className="fav-favs">
+                                    <i className="fa-solid fa-star" style={{ color: '#daab03' }}></i>
+                                </button>
+                            </div>
+                            <div className="botones-cont3">
+                                <button className="compra-favs">
+                                    <i className="fa-solid fa-cart-shopping" style={{ color: '#248cdb' }}></i>
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="carta-favs">
-                    <div className="img-favs-cont">
-                        <img className="img-favs" src="https://m.media-amazon.com/images/I/913fRR9Dk5L._UF894,1000_QL80_.jpg" />
-                    </div>
-                    <div className="cuerpo-favs">
-                        <h4>Título: Origins</h4>
-                        <p>Artista: Imagine Dragons </p>
-                        <p>Año: 2018 </p>
-                        <button type="button" className="genero-favs btn btn-danger">Pop-Rock</button>
-                    </div>
-                    <div className="botones-cont1">
-                        <div className="botones-cont2">
-                            <button className="fav-favs">
-                                <i class="fa-solid fa-star" style={{ color: '#daab03' }}></i>
-                            </button>
-                        </div>
-                        <div className="botones-cont3">
-                            <button className="compra-favs">
-                                <i class="fa-solid fa-cart-shopping" style={{ color: '#248cdb' }}></i>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                );
+            })}
         </div>
     )
 }
