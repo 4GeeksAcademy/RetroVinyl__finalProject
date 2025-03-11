12b12eda809e773d60db613f69c4a91b021aa5c5
Lucrecia-15 <parodilucrecia@gmail.com>
2025-03-11 21:41:18 +0000

    css en infoalbum

--- src/front/js/views/InfoAlbum.js
+++ src/front/js/views/InfoAlbum.js
@@ -9,13 +9,13 @@ export const InfoAlbum = () => {
 
     const [cantidad, setCantidad] = useState(1)
     const [precio, setPrecio] = useState(20)
-    const precioTotal= precio*cantidad;
+    const precioTotal = precio * cantidad;
 
-    const restar=((num) => {
-        if (num >1) {
-            return num-1
+    const restar = ((num) => {
+        if (num > 1) {
+            return num - 1
         }
-        else{
+        else {
             return 1
         }
     })
@@ -25,69 +25,71 @@ export const InfoAlbum = () => {
 
     const { albumid } = useParams();
     const [albums, setAlbums] = useState([])
-    
-        useEffect(()=>{ // DONDE LLAMO A LA RUTA DEL BACK QUE TRAE LA INFORMACION??????
-            console.log("estoy cargando los albumes");
-            
-            const getAlbums = async () =>{
-                const response = await fetch(`${process.env.BACKEND_URL}/api/infoAlbums/${albumid}`)
-                const data = await response.json()
-                setAlbums(data)
-    
-            }
-            getAlbums()
-        },[albumid])
-   
+
+    useEffect(() => { // DONDE LLAMO A LA RUTA DEL BACK QUE TRAE LA INFORMACION??????
+        console.log("estoy cargando los albumes");
+
+        const getAlbums = async () => {
+            const response = await fetch(`${process.env.BACKEND_URL}/api/infoAlbums/${albumid}`)
+            const data = await response.json()
+            setAlbums(data)
+
+        }
+        getAlbums()
+    }, [albumid])
+
     return (
         <div className="container">
             <div className="row p-4 justify-content-center">
-                
+
                 <div className="card-info card col-md-5">
-                <img src={albums.cover_image} className="img-card card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">Titulo: {albums.title}</h5>
-                    <p className="card-text">Pais: {albums.country} </p>
-                    <p className="card-text">Año: {albums.year}</p>
-                    <p className="card-text">Género: {albums.genre} </p>
-                </div>
-                <div className="card-body">
-                    <div className="d-flex justify-content-between">
-                        <p className="card-text">Cantidad: </p>
-                    <div class="d-sm-flex">
-                        <button class="btn btn-primary ms-1" type="button" onClick={() => setCantidad(restar(cantidad))}>-</button>
-                        <p className="card-text ps-3 pe-3">{cantidad} </p>
-                        <button class="btn btn-primary ms-1" type="button" onClick={() => setCantidad(cantidad + 1)}>+</button>
+                    <img src={albums.cover_image} className="img-card card-img-top" alt="..." />
+                    <div className="card-body ml-3">
+                        <h5 className="card-title mt-3">Titulo: {albums.title}</h5>
+                        <p className="card-text mt-3">Pais: {albums.country} </p>
+                        <p className="card-text">Año: {albums.year}</p>
+                        <p className="card-text">Género: <strong>{albums.genre}</strong> </p>
                     </div>
-                    </div>
-                    <p className="card-text">TOTAL: {precioTotal}€</p>
+                    <div className="card-body">
+                        <div className="d-flex justify-content-between">
+                            <p className="card-text mt-3"><strong>Cantidad:</strong></p>
+                            <div class="d-sm-flex">
+                                <button class="btn btn-outline-danger ms-1" type="button" onClick={() => setCantidad(restar(cantidad))}>-</button>
+                                <p className="card-text mt-3 ps-3 pe-3"><strong>{cantidad}</strong></p>
+                                <button class="btn btn-outline-danger ms-1" type="button" onClick={() => setCantidad(cantidad + 1)}>+</button>
+                            </div>
+                        </div>
+                        <br></br>
+                        <p className="card-text"><strong>TOTAL :</strong>{precioTotal}<strong>€</strong></p>
 
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        Comprar
-                    </button>
+                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Comprar
+                        </button>
 
-                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body text-black">
-                                    ...
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-primary">Confirmar compra</button>
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body text-black">
+                                        ...
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-outline-danger">Confirmar compra</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-             
-                
-                <div className="card-coment text-black border-start border-3 border-danger ms-3 col-md-6">
+
+
+                <div className="card-coment text-light text-center ms-3 col-md-6">
                     <div className="p-3">
-                        <h3>Comentarios</h3>
+                        <h3 className="sentimientosRetro" >"Sentimientos RetroVinyl"</h3>
+                        <h4> Lo Que Dicen los Fans</h4>
                     </div>
                     <div className="list-comments">
                         {commentList.map((text, index) => (
@@ -100,9 +102,10 @@ export const InfoAlbum = () => {
                         ))}
                     </div>
                     <div className="input-group mb-3">
-                        <input type="text" className="form-control bg-light" placeholder="Deja tu comentario"
+                        <input type="text" className="form-control" placeholder="Deja tu comentario"
                             onChange={(e) => setNewComment(e.target.value)}
-                            value={newComment} />
+                            value={newComment}
+                        />
                         <button className="btn btn-danger" type="button" onClick={() => { setCommentList(() => [...commentList, newComment]); setNewComment(''); }}>Añadir</button>
                     </div>
                 </div>
